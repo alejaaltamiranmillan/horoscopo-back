675c48cf88b6844344cc9893b4f62627191dc249
aleja <alejandra.altamiran01@unicatolica.edu.co>
2024-10-07 10:04:34 -0500

    123

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 const express = require('express');
-const {urlencoded, json} = require('express');
+const { urlencoded, json } = require('express');
 const router = require('./routes/signos.routes.js');
 const cors = require('cors');
 
 const app = express();
 
-app.use(urlencoded({extended: true}))
-app.use(json())
+app.use(urlencoded({ extended: true }));
+app.use(json());
 
 const corsOptions = {
-    origin: '*',
-    methods: 'GET, HEAD, PATCH, POST, PUT, DELETE',
-    allowedHeaders: ['Content-Type, Authorization']
+    origin: '*',  // Permitir acceso desde cualquier origen, pero en producción, ajusta a los dominios permitidos
+    methods: ['GET', 'HEAD', 'PATCH', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 };
 
-app.get('/', (req, res)=>{
-    res.send("hola mundo")
-})
+// Aplicamos CORS con las opciones definidas
+app.use(cors(corsOptions));
 
-app.use(cors())
+app.get('/', (req, res) => {
+    res.send("hola mundo");
+});
+
+// Rutas
 app.use('/api', router);
 
-app.listen(4001, ()=>{
-    console.log('listening at port 4001');
-})
+// Servidor en escucha
+app.listen(4001, () => {
+    console.log('Listening at port 4001');
+});
